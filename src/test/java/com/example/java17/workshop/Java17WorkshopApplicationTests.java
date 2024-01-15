7bbd036728c678369b41aef29a16c37104c8298d
Sercan ÇELENK <sercancelenk@gmail.com>
2024-01-15 08:52:33 +0300

    Update Java17WorkshopApplicationTests.java

--- src/test/java/com/example/java17/workshop/Java17WorkshopApplicationTests.java
+++ src/test/java/com/example/java17/workshop/Java17WorkshopApplicationTests.java
@@ -1,12 +1,11 @@
 package com.example.java17.workshop;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class Java17WorkshopApplicationTests {
 
-    @Test
+
     void contextLoads() {
     }
 
